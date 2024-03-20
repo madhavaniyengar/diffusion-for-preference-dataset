1a9a26e0921a1e0f3f26700c59a1005df447bc60
Basavasagar K Patil <sagarbasava8@gmail.com>
2024-03-20 16:02:58 +0000

    latest updates to ipynb

--- diffusion-features/utils/get_trajectories.py
+++ diffusion-features/utils/get_trajectories.py
@@ -11,8 +11,8 @@ def get_trajectories():
     """Get trajectories from the manual control json files.
     and return a tensor of coordinates for the whole batch of trajectories."""
     # read the json files
-    relative_path = '../../environment/data/lavaenv'
-    absolute_path = os.path.abspath(relative_path)
+    # relative_path = '../../environment/data/lavaenv'
+    absolute_path = '/teamspace/studios/this_studio/diffusion-features/environment/data/lavaenv'
     print(absolute_path)
     files = os.listdir(absolute_path)
     # sort the files according to name
@@ -21,16 +21,20 @@ def get_trajectories():
     files.pop(0)
     files.pop(-1)
     trajectories = {}
+    max = -1
     for file in files:
-        relative_path = f'../../environment/data/lavaenv/{file}/trajectories.json'
-        absolute_path = os.path.abspath(relative_path)
-        with open(absolute_path, 'r') as f:
+        absolute_path_ = os.path.join(absolute_path, file, 'trajectories.json')
+        with open(absolute_path_, 'r') as f:
             data = json.load(f)
             #print(file, data)
             trajectory = np.array(data['positions'])
             # remove the adjacent position duplicates
             trajectory = trajectory[~(np.roll(trajectory, 1, axis=0) == trajectory).all(axis=1)]
             trajectories[file] = trajectory
+            max = len(trajectory) if len(trajectory) > max else max
+    for key, trajectory in trajectories.items():
+        # pad the trajectories with zeros to make them of the same length
+        trajectories[key] = np.pad(trajectory, ((0, max - len(trajectory)), (0, 0)))
     # conver the values of the dictionary to tensors
     trajectories_tensor = torch.tensor([trajectory for trajectory in trajectories.values()])
     return trajectories_tensor
@@ -50,6 +54,19 @@ def visualize_trajectories(trajectories):
         plt.gca().invert_yaxis()
         plt.savefig(f'../../environment/data/lavaenv/{key}/trajectory.png')
 
+def visualize_trajectory(trajectory):
+    """Visualize the trajectory."""
+    trajectory = trajectory.numpy()
+    trajectory = trajectory.squeeze()
+    x = [point[0] for point in trajectory]
+    y = [point[1] for point in trajectory]
+    # plot the figure using matplotlib with origin at the top left
+    plt.figure()
+    plt.scatter(x, y)
+    plt.xlim(0, 8)
+    plt.ylim(0, 8)
+    plt.gca().invert_yaxis()
+    plt.show()
  
 
 def main():
