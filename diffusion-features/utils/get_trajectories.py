9e95d4184fceefc5cf8494e27e06cfd95e078cb2
Basavasagar K Patil <sagarbasava8@gmail.com>
2024-03-20 02:50:30 +0000

    plot trajectories

--- diffusion-features/utils/get_trajectories.py
+++ diffusion-features/utils/get_trajectories.py
@@ -16,24 +16,30 @@ def get_trajectories():
     # delete the first and last file
     files.pop(0)
     files.pop(-1)
-    trajectories = []
+    trajectories = {}
     for file in files:
         with open(f'../../environment/data/lavaenv/{file}/trajectories.json', 'r') as f:
             data = json.load(f)
             #print(file, data)
-            trajectories.append(data['positions'])
+            trajectories[file] = data['positions']
     return trajectories
 
 
 def visualize_trajectories(trajectories):
     """Visualize the trajectories."""
     print(trajectories)
-    for trajectory in trajectories:
+    for key, trajectory in trajectories.items():
         x = [point[0] for point in trajectory]
         y = [point[1] for point in trajectory]
+        # plot the figure using matplotlib with origin at the top left
+        plt.figure()
         plt.plot(x, y)
-        plt.savefig(f'../../environment/data/lavaenv/trajectories.png')
+        plt.xlim(0, 8)
+        plt.ylim(0, 8)
+        plt.gca().invert_yaxis()
+        plt.savefig(f'../../environment/data/lavaenv/{key}/trajectory.png')
 
+ 
 
 def main():
     trajectories = get_trajectories()
